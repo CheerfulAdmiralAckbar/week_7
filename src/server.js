caab8e672a2f18263abb03636e792734dc76d6f4
Ben Johnston <admiralackbar411@gmail.com>
2024-07-01 13:50:01 +0100

    Removed static express sites

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-
-app.use("/batman", express.static("batman"));
-
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
 });
