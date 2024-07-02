54ec7f34bc3aa1b6a399c3a8d200ee1fcbad6294
Ben Johnston <admiralackbar411@gmail.com>
2024-07-02 14:45:41 +0100

    CRUD routes create that will change mongoose database

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -5,13 +6,77 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/books/getAllBooks", (request, response) => {});
+console.log("HELLO_WORLD: " + process.env.HELLO_WORLD);
 
-app.post("/books/addBook", (request, response) => {});
+const connection = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Connected to MongoDB");
+};
 
-app.put("/books", (request, response) => {});
+connection();
 
-app.delete("/books", (request, response) => {});
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+app.get("/books/getAllBooks", async (request, response) => {
+  const books = await Book.find();
+  response.send(books);
+});
+
+app.post("/books/addBook", async (request, response) => {
+  const book = await Book.create({
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  });
+
+  const successResponse = {
+    message: "success",
+    book: book,
+  };
+
+  response.send(successResponse);
+});
+
+// Update Book by title
+app.put("/books", async (request, response) => {
+  console.log(request.body);
+  const updateAuthorByTitle = await Book.updateOne(
+    { title: request.body.title },
+    { author: request.body.author }
+  );
+
+  const successResponse = {
+    message: "success",
+    book: updateAuthorByTitle,
+  };
+  response.send(successResponse);
+});
+
+// Delete Book by title
+app.delete("/books", async (request, response) => {
+  const deleteBookByTitle = await Book.deleteOne({ title: request.body.title });
+
+  const successResponse = {
+    message: "success",
+    book: deleteBookByTitle,
+  };
+  response.send(successResponse);
+});
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
